20b51b617e34d4eb65270f3bd212d2b53bd4b520
Adrianna Brown <adrianna.brown@gatech.edu>
2018-02-19 10:36:17 -0500

    stubbed reg feature

--- app/src/main/java/edu/gatech/team10/weshelter/User.java
+++ app/src/main/java/edu/gatech/team10/weshelter/User.java
@@ -1,27 +1,33 @@
 package edu.gatech.team10.weshelter;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Ben on 2/11/2018.
  */
 
 public class User {
     private String name;
-    private String email;
+    private String username;
     private String password;
+    private String type;
+    public static List<String> legalTypes = Arrays.asList("User", "Admin");
 
     User(){
-        this("user", "password", "name");
+        this("user", "password", "name", "User");
     }
-    User(String email, String password, String name){
-        this.email = email;
+    User(String username, String password, String name, String type){
+        this.username = username;
         this.password = password;
         this.name = name;
+        this.type = type;
     }
     public String getPassword(){
         return password;
     }
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
 
     public String getName() {
@@ -32,11 +38,33 @@ public class User {
         this.password = password;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) { return false; }
+        if (this == other) { return true; }
+        if (!(other instanceof User)) {return false; }
+        User o = (User) other;
+        if (o.username.equals(this.username)) { return true; }
+        else { return false; }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        for (int i = 0; i < this.username.length(); i++) {
+            hash = hash*31 + this.username.charAt(i);
+        }
+        return hash;
     }
 }
